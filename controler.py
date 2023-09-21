3acec26823886c5ff5ecc5ae04e538440c6836e2
lealand <1513563570@qq.com>
2023-09-21 23:28:43 +0800

    新文件：   .env .template 	新文件：   .gitignore 	修改：     controler.py 	新文件：   modules.json

--- controler.py
+++ controler.py
@@ -1,1 +1,14 @@
 # 核心控制模块
+import openai
+import os
+import json
+from dotenv import load_dotenv
+
+
+# import env variables
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_KEY')
+
+# description of each module
+with open('modules.json', "r") as f:
+    module_descriptions = json.load(f) 
