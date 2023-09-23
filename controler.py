382d7a773aa905c78eef52255fc2d704481dfe5e
lealand <1513563570@qq.com>
2023-09-23 17:11:36 +0800

    修改：     .env .template 	修改：     .gitignore 	修改：     controler.py

--- controler.py
+++ controler.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 
 # import env variables
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # description of each module
 with open('modules.json', "r") as f:
     module_descriptions = json.load(f) 
+
+
