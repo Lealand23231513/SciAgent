fac97b38dad18ed2f4e397c06ad5637d5d01dfde
lealand <1513563570@qq.com>
2023-10-01 22:25:55 +0800

    renamed:    ResearchStatus_Pdh/ResearchStatus -> ResearchStatus.py 	new file:   ResearchStatus_Pdh/ResearchStatus.py 	new file:   search_and_download.py

---
+++ ResearchStatus_Pdh/ResearchStatus.py
@@ -0,0 +1,76 @@
+##用jupyter notebook写的 先直接放上来了（（
+pip install langchain
+pip install arxiv
+pip install pymupdf
+pip install openai
+import os
+os.environ["OPENAI_API_KEY"] = "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 "
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import arxiv
+def PaperGet():
+  print("开始查找论文"+topic)
+  search = arxiv.Search(
+  query = topic,
+  max_results = 10,
+  sort_by = arxiv.SortCriterion.Relevance
+)
+  for result in search.results():
+    paper_url=paper_url+result.entry_id
+    print(result.title)
+    print(result.entry_id)  
+def ResearchStatus():
+    llm=OpenAI(temperature=0.0)
+    print(paper_url)
+    prompt=PromptTemplate(
+        input_variables=["topic"],
+        template="You are an authoritative writer in the field of {topic}, \
+        and you are reading several papers' abstracts , the urls of the papers are here:\
+        {paper_url} .Please write an overview, about 500 word, of the current research situation in the field\
+        according to the abstracts, and it should be concise, clear and easy to understand, in line with the original meaning,\
+        and at the same time needs to include the history and current situation of the research on this topic, \
+        the main theoretical views and techniques at this stage, the main direction of the topic, \
+        the main problems need to be solved urgently and the main development trends. "
+    )
+    chain=LLMChain(llm=llm,prompt=prompt)
+    print("I'm working now!")
+    result1=chain.run(topic)
+    return result1
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+ )
+def ChineseTranslation(TOPIC):
+    chat = ChatOpenAI(temperature=0)
+    template="You are a helpful assistant that translates {input_language} to {output_language}. For the already English part in\
+              input, you should not change it.For example, if you get an input like 'AI数据结构'，you should output like this\
+              'AI data structure'."
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template="{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    topic=chat(chat_prompt.format_prompt(input_language="Chinese", output_language="English", text=TOPIC).to_messages()).content
+    print(topic)
+    return topic
+def EnglishTranslation(TOPIC):
+    chat = ChatOpenAI(temperature=0)
+    template="You are a helpful assistant that translates {input_language} to {output_language}.."
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template="{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    topic=chat(chat_prompt.format_prompt(input_language="English", output_language="Chinese", text=TOPIC).to_messages()).content
+    print(topic)
+    return topic
+topic=input("输入需要查找的论文领域：")
+print("我明白了。你需要查找的论文领域是"+topic)
+topic=ChineseTranslation(topic)
+print("翻译成了英文"+topic)
+PaperGet()
+result=ResearchStatus()
+result=EnglishTranslation(result)
+print(result)
+print("工作结束了!")
---
+++ search_and_download.py
@@ -0,0 +1,137 @@
+"""
+    这个文件里有两个函数: arxiv_auto_search_and_download(top_k_results)和arxiv_auto_search(top_k_results)
+    第一个函数提供搜索且下载的功能, 第二个函数仅搜索
+    top_k_results参数控制返回文论文的数量
+    两个函数的返回参数都是一个python字典, 都包含'title'和'arxiv_id'(字面意思)
+"""
+import arxiv
+import requests
+import openai
+import json
+import os
+from typing import Any, Dict, List, Optional, Union, ClassVar
+
+from pydantic import BaseModel, root_validator
+from langchain.schema import Document
+
+
+#TODO:将翻译集成到ArxivAPIWrapper中
+class ArxivAPIWrapper(BaseModel):
+    arxiv_exceptions:tuple = (
+                arxiv.ArxivError,
+                arxiv.UnexpectedEmptyPageError,
+                arxiv.HTTPError,
+            ) 
+    arxiv_result:ClassVar = arxiv.Result
+    top_k_results: int = 3
+    ARXIV_MAX_QUERY_LENGTH: int = 300
+    load_max_docs: int = 100
+    load_all_available_meta: bool = False
+    doc_content_chars_max: Optional[int] = 40000
+
+    def run(self, query: str) -> list[dict[str, str]] | str:
+        """
+        Performs an arxiv search and A single string
+        with the publish date, title, authors, and summary
+        for each article separated by two newlines.
+
+        If an error occurs or no documents found, error text
+        is returned instead. Wrapper for
+        https://lukasschwab.me/arxiv.py/index.html#Search
+
+        Args:
+            query: a plaintext search query
+        """  # noqa: E501
+        try:
+            results = arxiv.Search(  
+                query[: self.ARXIV_MAX_QUERY_LENGTH], max_results=self.top_k_results
+            ).results()
+        except self.arxiv_exceptions as ex:
+            return f"Arxiv exception: {ex}"
+        docs = [
+            {
+                "Title": result.title,
+                "arxiv_id": result.entry_id[21:]
+            }
+            for result in results
+        ]
+
+        if docs:
+            return docs
+        else:
+            return "No good Arxiv Result was found"
+
+def download_arxiv_pdf(arxiv_id, ori_name:str, folder_name:str):
+    pdf_url = 'https://arxiv.org/pdf/' + arxiv_id + '.pdf'
+    response = requests.get(pdf_url)
+
+    if response.status_code == 200:
+        # 定义一个字符映射表并创建翻译表，用来替换文件名中不能出现的字符
+        char_mapping = {
+            '\\': ' ',
+            '/': ' ',
+            '?': ' ',
+            ':': ' ',
+            '<': ' ',
+            '>': ' ',
+            '|': ' ',
+            '*': ' ',
+            '"': ' ',
+        }
+        translation_table = str.maketrans(char_mapping)
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        filename = os.path.join(folder_name.translate(translation_table), ori_name.translate(translation_table) + '.pdf')
+
+        with open(filename, 'wb') as pdf_file:
+            pdf_file.write(response.content)
+        print(f'文件 {filename} 下载成功！')
+    else:
+        print(f'下载失败，HTTP状态码: {response.status_code}')
+
+# def get_completion(prompt, model="gpt-3.5-turbo"):
+#     messages = [{"role": "user", "content": prompt}]
+#     response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=messages,
+#         temperature=0,
+#     )
+#     return response.choices[0].message["content"]
+
+def arxiv_auto_search_and_download(query:str, download:bool = False, top_k_results=3) -> list[dict[str, str]] | str | None:
+
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    # 获取用户输入并翻译成英文
+
+    # query = input(f"您好! 我是您的论文下载助手。\n\
+    # 请输入您想下载的论文的所属领域(单词即可，可以使用任意一种语言)，我会自动查阅在Arxiv托管的论文中的相关论文并为您下载相关度最高的{top_k_results}篇(如果有那么多的话)到当前目录：\n")
+
+    # prompt = f"""
+    # 请你将该单词翻译成英文，并且只返回翻译后的英文单词：{query}
+    # """
+    # question = get_completion(query)
+
+    arxiv_wrapper = ArxivAPIWrapper(top_k_results=top_k_results)
+    arxiv_result = arxiv_wrapper.run(f"""{query}""")
+
+    if type(arxiv_result) == str:
+        print(arxiv_result)
+        return None
+    
+    
+    print("get results:")
+    for i,sub_dict in enumerate(arxiv_result):
+        print(f"{i+1}.{json.dumps(sub_dict)}")
+
+    # 如果不下载，直接返回
+    if download == False:
+        return arxiv_result
+
+    # 循环遍历result中的每个结果并下载论文
+    for sub_dict in arxiv_result:
+        if type(sub_dict) == dict:
+            download_arxiv_pdf(sub_dict["arxiv_id"], sub_dict["Title"], query)
+
+    return arxiv_result
