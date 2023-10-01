acdf806ee0b3de6468ba8c131dd94b3a7fb63dc8
lealand <1513563570@qq.com>
2023-10-01 17:29:02 +0800

    modified:   .gitignore 	new file:   ArxivAutoSearchAndDownload.ipynb 	modified:   controler.py 	new file:   summary.py

--- controler.py
+++ controler.py
@@ -11,6 +11,7 @@ from langchain.prompts.chat import (
 )
 from langchain.chains import LLMChain
 from langchain.schema import BaseOutputParser
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
 # import env variables
 load_dotenv()
@@ -20,20 +21,36 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 with open('modules.json', "r") as f:
     module_descriptions = json.load(f) 
 
-
 user1_input = "I want to find some papers about LLM."
 user2_input = "I want to write a review about LLM, and I wonder what papers can I refer to? And please write me a summary about the papers."
-output_format = [
-    {
-        "name" : 'module\'s name',
-        "function" : 'module\'s function name'
-    },
-    {
-        "name" : 'module\'s name',
-        "function" : 'module\'s function name'
-    }
-]
 
+
+
+# define output_parser
+class ModuleOutputParser(BaseOutputParser):
+    def get_format_instructions(self) -> str:
+        output_format = [
+                            {
+                                "name" : 'module\'s name',
+                                "function" : 'module\'s function name'
+                            },
+                            {
+                                "name" : 'module\'s name',
+                                "function" : 'module\'s function name'
+                            },
+                            {
+                                "name" : 'module\'s name',
+                                "function" : 'module\'s function name'
+                            }
+                        ]
+        return json.dumps(output_format)
+    def parse(self, text: str) :
+        #TODO: Add check
+        # expected_keys = [rs.name for rs in self.response_schemas]
+        return json.loads(text)
+output_parser = ModuleOutputParser()
+
+# define chat_prompt
 template = "You are a helpful assistant that can choose which module to execute for the user's input.\
       The modules information and function is in json format as below:\n{module_descriptions}.\
       A user will pass in a querry, and you should generate module name, function to execute in json format as output format below according to the modules information. The number of modules is probably more than 1.\
@@ -42,15 +59,22 @@ system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 human_template = "{text}"
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-
+chat_prompt =  ChatPromptTemplate(
+    messages=[system_message_prompt, human_message_prompt],
+    input_variables=["text"],
+    partial_variables={
+        "module_descriptions": str(module_descriptions),
+        "output_format": output_parser.get_format_instructions()
+        }
+)
 
 chain = LLMChain(
     llm=ChatOpenAI(temperature=0),
-    prompt=chat_prompt
+    prompt=chat_prompt,
+    output_parser=output_parser
 )
-response = chain.run(text=user2_input, module_descriptions=module_descriptions, output_format = output_format)
-print(response)
-#TODO: Change response into JSON format with langchain method OutputParser. 
+response = chain.run(user2_input)
+
+
+
 #TODO: Contact controler to other modules.
---
+++ summary.py
@@ -0,0 +1,37 @@
+from langchain.llms import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader
+from langchain.prompts import PromptTemplate
+
+import os
+import sys
+os.environ["OPENAI_API_KEY"] = "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"
+
+path = input("输入你要进行概括的论文路径（支持pdf和word版）:")
+if(path.split(".")[-1] == 'pdf'):
+    loader = PyPDFLoader(path)
+elif(path.split(".")[-1] == 'docx'):
+    loader = Docx2txtLoader(path)
+else:
+    print("论文文件格式错误")
+    sys.exit()
+
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+print(f'documents:{len(docs)}')
+
+prompt_template = """Write a summary of this paper,
+which should contain introduction of research field, process and achievements:
+ 
+{text}
+ 
+SUMMARY IN Chinese:"""
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+llm = OpenAI(temperature=0.2, max_tokens=1000)
+chain = load_summarize_chain(llm, chain_type="map_reduce", return_intermediate_steps=False, map_prompt=PROMPT, combine_prompt=PROMPT)
+summary = chain({"input_documents": docs}, return_only_outputs=True)["output_text"]
+print(summary)
