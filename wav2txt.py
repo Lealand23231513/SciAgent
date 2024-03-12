786f12b46e659919102d6780dcb6f3e3cbbdfcf7
Fripping <15010770306@163.com>
2024-03-12 17:11:57 +0800

    try

--- wav2txt.py
+++ wav2txt.py
@@ -1,5 +1,5 @@
-import paddlespeech
 from paddlespeech.cli.asr.infer import ASRExecutor
+from paddlespeech.server.bin.paddlespeech_client import ASRClientExecutor
 
 def wav2txt(wav_path: str, lang: str='zh', sample_rate: int=16000):
     asr = ASRExecutor()
@@ -7,8 +7,20 @@ def wav2txt(wav_path: str, lang: str='zh', sample_rate: int=16000):
         model = 'conformer_wenetspeech'
     elif lang=='en':
         model = 'transformer_librispeech'
-    result = asr(audio_file=wav_path, 
-                 model=model,
-                 lang=lang,
-                 sample_rate=sample_rate)
+    result = asr(
+        audio_file=wav_path, 
+        model=model,
+        lang=lang,
+        sample_rate=sample_rate)
+    return result
+
+def wav2txt_client(wav_path: str, server_ip: str='127.0.0.1', port: int=8090, lang: str='zh', sample_rate: int=16000):
+    asrclient_executor = ASRClientExecutor()
+    result = asrclient_executor(
+        input=wav_path,
+        server_ip=server_ip,
+        port=port,
+        sample_rate=sample_rate,
+        lang=lang,
+        audio_format="wav")
     return result
